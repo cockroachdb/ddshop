0a17600e805eea11d879858ec86b93c571156d85
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-09-14 15:37:17 -0400

    logging

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -24,7 +25,13 @@ type server struct {
 	db *robustdb.DB
 }
 
+func writeError(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+	log.Printf("error while handling request: %s", err)
+}
+
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", r.Method, r.URL)
 	if !strings.HasPrefix(r.URL.Path, "/api") {
 		http.ServeFile(w, r, filepath.Join(cwd, "assets", r.URL.Path))
 		return
@@ -37,31 +44,31 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		todos, err := listTodos(s.db)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 		data, err := json.Marshal(todos)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 		if _, err := w.Write(data); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 	case "POST":
 		var t todo
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 		if err := json.Unmarshal(body, &t); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 		if err := upsertTodo(s.db, t); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			writeError(w, err)
 			return
 		}
 	default:
