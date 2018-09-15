a23456d390ef0f04418453852f6da0a3904e3ebc
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2018-09-15 13:22:38 -0400

    conn timeout

--- robustdb/robustdb.go
+++ robustdb/robustdb.go
@@ -1,28 +1,39 @@
 // Package robustdb implements a database client that can tolerate failures.
 package robustdb
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type DB struct {
-	urls   []string
-	sqlDBs []*sql.DB
+	urls  []string
+	conns []*sql.Conn
 }
 
 func New(urls ...string) (*DB, error) {
+	ctx := context.TODO()
 	db := &DB{urls: urls}
 	for _, u := range urls {
 		sqlDB, err := sql.Open("postgres", u)
 		if err != nil {
 			return nil, err
 		}
-		db.sqlDBs = append(db.sqlDBs, sqlDB)
+		conn, err := sqlDB.Conn(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if _, err := conn.ExecContext(ctx, `SET statement_timeout = '200ms'`); err != nil {
+			return nil, err
+		}
+		db.conns = append(db.conns, conn)
 	}
 	return db, nil
 }
 
-func (db *DB) retry(fn func(sqlDB *sql.DB) error) error {
+func (db *DB) retry(fn func(conn *sql.Conn) error) error {
 	var err error
-	for _, sqlDB := range db.sqlDBs {
+	for _, sqlDB := range db.conns {
 		if err = fn(sqlDB); err == nil {
 			return err
 		}
@@ -31,16 +42,16 @@ func (db *DB) retry(fn func(sqlDB *sql.DB) error) error {
 }
 
 func (db *DB) Ping() error {
-	return db.retry(func(sqlDB *sql.DB) error {
-		return sqlDB.Ping()
+	return db.retry(func(conn *sql.Conn) error {
+		return conn.PingContext(context.TODO())
 	})
 }
 
 func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
 	var result sql.Result
-	err := db.retry(func(sqlDB *sql.DB) error {
+	err := db.retry(func(conn *sql.Conn) error {
 		var err error
-		result, err = sqlDB.Exec(query, args...)
+		result, err = conn.ExecContext(context.TODO(), query, args...)
 		return err
 	})
 	return result, err
@@ -48,9 +59,9 @@ func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
 
 func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
 	var rows *sql.Rows
-	err := db.retry(func(sqlDB *sql.DB) error {
+	err := db.retry(func(conn *sql.Conn) error {
 		var err error
-		rows, err = sqlDB.Query(query, args...)
+		rows, err = conn.QueryContext(context.TODO(), query, args...)
 		return err
 	})
 	return rows, err
