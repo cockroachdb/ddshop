2d681687e0b1c1816091c4f8c546e7897368f713
Pete Vilter <vilterp@cockroachlabs.com>
2018-09-15 14:30:50 -0400

    import UI from github.com/xtrinch/go-react-todo

---
+++ ui/src/components/todo.jsx
@@ -0,0 +1,73 @@
+import React, { PropTypes } from 'react'
+
+const Todo = ({ onClick, deleteTodo, updateTodo, todo }) => (
+  <li>
+    <div className="view">
+
+      <input
+        className="toggle"
+        type="checkbox"
+        checked={todo.completed}
+        onClick={(evt) => {
+          evt.preventDefault();
+          todo.completed = !todo.completed;
+          updateTodo();
+        }}
+      />
+      <label>{todo.title}</label>
+      <button
+        className="destroy"
+        onClick={(evt) => {
+          evt.preventDefault();
+          deleteTodo();
+        }}
+      />
+
+      {/* <span
+        style={{
+          textDecoration: todo.completed? 'line-through': 'none'
+        }}
+        onClick={onClick}
+      >
+        {todo.title}
+      </span>
+
+      <a
+        href="#"
+        style={{
+          marginLeft: "10px",
+          textDecoration: todo.completed? 'line-through': 'none'
+        }}
+        onClick={e => {
+          e.preventDefault()
+          todo.completed = !todo.completed
+          updateTodo()
+        }}
+      >
+          (complete)
+      </a>
+
+      <a
+        href="#"
+        style={{
+          marginLeft: "10px"
+        }}
+        onClick={e => {
+            e.preventDefault()
+            deleteTodo()
+        }}
+      >
+        (delete)
+      </a> */}
+    </div>
+  </li>
+)
+
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+  updateTodo: PropTypes.func.isRequired,
+  todo: PropTypes.object.isRequired
+}
+
+export default Todo
